fc24d8a4d9bc3db44bd516bbe4fc16a7b0b0718d
IshaParihariya <ishh1036@gmail.com>
2026-05-17 19:42:04 +0530

    first commit

---
+++ src/main/java/com/isha/springdatajpamongodb/SpringDataJpaMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.isha.springdatajpamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isha/springdatajpamongodb/SpringDataJpaMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isha.springdatajpamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
